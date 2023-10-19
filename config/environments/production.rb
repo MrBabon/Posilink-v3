fec06ce01af4454571f12e472b141b5a998509a8
MrBabon <christophedanna06@gmail.com>
2023-10-19 17:17:51 +0200

    test avec host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "https://my-task-    tracker.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "www.dannacode.com" }
   # Settings specified here will take precedence over those in config/application.rb.
   ##########################################################
   ############### POUR FORMULAIRE DE CONTACT ###############
