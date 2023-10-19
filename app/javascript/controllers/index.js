c84aea806c6082b9ca0d0d1b6a02a05ea6c226cc
MrBabon <christophedanna06@gmail.com>
2023-10-19 15:59:31 +0200

    index.js de controllers modifie

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,20 +6,6 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import ButtonPhoneController from "./button_phone_controller"
-application.register("button-phone", ButtonPhoneController)
-
-import MapController from "./map_controller"
-application.register("map", MapController)
-
-import ScrollController from "./scroll_controller"
-application.register("scroll", ScrollController)
-
-import ReadMoreController from "./read_more_controller"
-application.register("read-more", ReadMoreController)
-
-import CurrentYearController from "./current_year_controller"
-application.register("current-year", CurrentYearController)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
